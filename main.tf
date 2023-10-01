9c28aa38d47f798a8a685bb377885b6cd480299f
Vincent Genest <vincent.genest@protonmail.com>
2023-10-01 12:02:50 -0400

    fix 3

--- main.tf
+++ main.tf
@@ -40,10 +40,17 @@ resource "azurerm_network_interface" "nic_test01" {
 }
 
 resource "azurerm_ssh_public_key" "hellgated" {
-  name                = "hellgateED"
+  name                = "hellgateed"
   resource_group_name = "dev"
   location            = "canada central"
   public_key          = file("HellgateED.pub")
+}
+
+resource "azurerm_ssh_public_key" "hellgatrsa" {
+  name                = "hellgatersa"
+  resource_group_name = "dev"
+  location            = "canada central"
+  public_key          = file("HellgateRSA.pub")
 }
 
 resource "azurerm_linux_virtual_machine" "test01_vm" {
@@ -58,7 +65,7 @@ resource "azurerm_linux_virtual_machine" "test01_vm" {
 
   admin_ssh_key {
     username   = "delta"
-    public_key = file("HellgateED.pub")
+    public_key = file("HellgateRSA.pub")
   }
 
   os_disk {
